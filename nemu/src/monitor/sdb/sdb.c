b9f97fffeddd0b6b9255343b4489256c276ab1f8
tracer-ics2021 <tracer@njuics.org>
2022-02-10 16:55:02 +0800

    >  compile 20215220703    Static hostname: ten          Icon name: computer-vm            Chassis: vm         Machine ID: 3938128b0cef1746b2f566d6ba1abca4            Boot ID: 6f3f90b3d1e64027832671f5c0cf73c9     Virtualization: kvm   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  16:55:02 up  4:14, 14 users,  load average: 0.29, 0.36, 0.28

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -59,8 +59,9 @@ static int cmd_si(char *args) {
 
 static int cmd_info(char *args) {
 	char *arg = strtok(NULL," ");
+	
 
-	if(*arg == 'r'){
+	if(strcmp(arg,"r")){
 	isa_reg_display();
 	}
   return 0;
